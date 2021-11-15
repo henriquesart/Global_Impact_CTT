db48f85bbd0371d7674beeb1e0394f1d0fdc853a
Henrique S <rick.sartal@hotmail.com>
2021-11-15 17:50:01 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fiap/zerowaste/ZerowasteApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.zerowaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerowasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerowasteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/zerowaste/ZerowasteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.zerowaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerowasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
